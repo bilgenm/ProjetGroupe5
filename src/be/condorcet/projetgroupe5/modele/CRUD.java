1770ae41425dabb41733f4c67ba4c3af6fe56478
bilgenm <bilgen.mustafa@condorcet.be>
2014-11-04 09:41:46 +0100

    Classe joueurBD et lieuDB

---
+++ src/be/condorcet/projetgroupe5/modele/CRUD.java
@@ -0,0 +1,8 @@
+package be.condorcet.projetgroupe5.modele;
+
+public interface CRUD {
+	  public void create()throws Exception;
+	  public void read() throws Exception;
+	  public void update() throws Exception;
+	  public void delete() throws Exception;
+}
